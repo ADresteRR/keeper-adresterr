5d79339daad09882179f10ff4c7909007b0b4fc0
ADresteRR <adresterrjr001@gmail.com>
2023-03-05 21:27:55 +0530

    after part 1 of keeper app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    const date = new Date();
+    return (
+        <footer>
+            <p>Copyright ⓒ {date.getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
